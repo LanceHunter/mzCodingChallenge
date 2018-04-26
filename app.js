50dca79d266f01bf293346769c43c2b320f5ceaf
Lance Hunter <lancehunter@gmail.com>
2018-04-26 08:05:44 -0500

    Commented on code.

--- app.js
+++ app.js
@@ -22,18 +22,20 @@ config[0].apiKey = process.env.SUNRISEBANKAPI;
 config[1].apiKey = process.env.HAPPYCREDITUNIONAPI;
 config[2].apiKey = process.env.PARISFCUAPI;
 
-// Getting bodyParser running.
+// Getting bodyParser to check the json body.
 app.use(bodyParser.json());
 
-// Route for GET requests, getting the information from request body and sending the reply as needed.
+// Route for POST requests, getting the information from request body and sending the reply as needed.
 app.post('/', (req, res) => {
-  console.log('Req.body - ', req.body);
+  // Verifying that all necessary inputs were provided in request body.
   if (!req.body.customer || !req.body.latitude || !req.body.longitude) {
-    res.status(400).send('Customer name, latitude, and longitude requred. Please send these in the URL of the request using the keys "customer", "latitude", and "longitude".');
+    res.status(400).send('Customer name, latitude, and longitude requred. Please send these in a JSON body using the keys "customer", "latitude", and "longitude".');
     return;
   }
+  // Parsing the latitude and longitude are numbers in case they got sent as strings.
   let latitude = parseFloat(req.body.latitude);
   let longitude = parseFloat(req.body.longitude);
+  // Making sure the latitude and longitude provided are valid.
   if (latitude > 90 || latitude < -90 || isNaN(latitude)) {
     res.status(400).send('Latitude is not valid.');
     return;
@@ -46,42 +48,48 @@ app.post('/', (req, res) => {
   let customerConfig = config.filter((customer) => {
     return customer.name.toUpperCase() === req.body.customer.toUpperCase();
   });
-  // If we were unable to find that customer name in the config file...
+  // If we were unable to find that customer name in the config file, send an error.
   if (!customerConfig[0]) {
     res.status(400).send('Unable to find information for that customer.');
     return;
   }
-  // With all verification completed. Now we get to actually start sending the request...
+  // With all verification completed. Now we get to actually start sending the request. First, we set up the search URL.
   let urlToGet = `https://maps.googleapis.com/maps/api/place/radarsearch/json?location=${latitude},${longitude}&radius=2000&language=${customerConfig[0].language}&type=${customerConfig[0].type}&key=${customerConfig[0].apiKey}`;
-
+  // Now we send the HTTPS request.
   https.get(urlToGet, (getRes) => {
+    // Getting the statuscode of the reply, and if it isn't a 200 then passing that forward.
     const { statusCode } = getRes;
-    console.log('statusCode - ', statusCode);
-
     if (statusCode !== 200) {
-      res.sendStatus(statusCode);
+      res.status(statusCode).send('Error received when sending request.');
       return;
     }
 
+    // If we got a 200, set the encoding and collect the reply data.
     getRes.setEncoding('utf8');
     let rawData = '';
     getRes.on('data', (chunk) => { rawData += chunk; });
+    // Once response is complete, try parsing the data.
     getRes.on('end', () => {
       try {
         let replyJSON = JSON.parse(rawData);
-        console.log('replyJSON.status - ', replyJSON.status);
+        // Verify that the response status received from Google is OK.
         if (replyJSON.status === "OK") {
+          // Get the results provided from Google in an array.
           let resultsArray = replyJSON.results;
+          // If the array of replies is longer than the request number in customer's config, remove extra items from array.
           if (resultsArray.length > customerConfig[0].requestNumber) {
             resultsArray = resultsArray.slice(0, customerConfig[0].requestNumber);
           }
+          // If the customer wants a response in JSON, send the reply in json.
           if (customerConfig[0].responseOutput === 'json') {
             res.status(200).send(resultsArray);
           } else {
+            // If the customer doesn't want a response in JSON, parse the data into XML and send that XML to customer.
             let builder = new xml2js.Builder();
             let xmlReply = builder.buildObject(resultsArray);
             res.status(200).send(xmlReply);
           }
+        // If the response status received from Google is not 'OK', send an error forward explaining the problem.
         } else if (replyJSON.status === 'ZERO_RESULTS') {
           res.status(200).send('No results for this location.');
         } else if (replyJSON.status === 'OVER_QUERY_LIMIT') {
@@ -96,13 +104,15 @@ app.post('/', (req, res) => {
           res.status(500).send('Unknown error received.');
         }
       } catch (e) {
+        // Catching any other errors in response.
         console.error(e.message);
       }
     });
   }).on('error', (e) => {
+    // Catching any other errors in HTTPS request.
     console.error(`Got error: ${e.message}`);
+    res.status(500).send(`Got error: ${e.message}`);
   });
-
 });
 
 app.listen(port, () => {

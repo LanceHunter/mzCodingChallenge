92c23a6ecfc22ee66a70d58b96d74752cda8589d
Lance Hunter <lancehunter@gmail.com>
2018-04-25 19:07:28 -0500

    Server set, now coding the route.

--- app.js
+++ app.js
@@ -6,4 +6,38 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8888;
 
+const config = {
+  sunriseBank : {
+    apiKey : process.env.SUNRISEBANKAPI,
+    type : 'atm',
+    language : 'English',
+    responseOutput : 'xml',
+    requestNumber : 200
+  },
+  happyCreditUnion : {
+    apiKey : process.env.HAPPYCREDITUNIONAPI,
+    type : 'bank',
+    language : 'Spanish',
+    responseOutput : 'json',
+    requestNumber : 20
+  },
+  parisFCU : {
+    apiKey : process.env.PARISFCUAPI,
+    type : 'all',
+    language : 'French',
+    responseOutput : 'json',
+    requestNumber : 5
+  }
+};
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(port, () => {
+  console.log('Listening on port ', port);
+});
+
 module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+
+/*
+describe('Array', () => {
+  describe('#indexOf()', () => {
+    it('should return -1 when a value is not present', () => {
+      assert.equal([1,2,3].indexOf(4), -1);
+      assert.equal([1,2,3].indexOf(5), -1);
+      assert.equal([1,2,3].indexOf(6), -1);
+      assert.equal([1,2,3].indexOf(7), -1);
+      assert.equal([1,2,3].indexOf(8), -1);
+    });
+    it('should return the correct index of item in array', () => {
+      assert.equal([1,2,3].indexOf(1), 0);
+      assert.equal([1,2,3].indexOf(2), 1);
+      assert.equal([1,2,3].indexOf(3), 2);
+    });
+  });
+});
+*/
